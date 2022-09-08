2adbfe1055f4272a03b4ed9d44c83681e69ca2b4
Tyler Felgenhauer <tfelgs77@gmail.com>
2022-09-08 17:06:22 -0500

    Home Page edits

--- reactCode/src/App.js
+++ reactCode/src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import "semantic-ui-css/semantic.min.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import Message from "./components/Message";
 
 function App() {
   return (
     <div>
       <div>
+        <Message />
         <NavBar />
         <Footer />
       </div>
--- reactCode/src/pages/Home.js
+++ reactCode/src/pages/Home.js
@@ -1,34 +1,42 @@
 import "semantic-ui-css/semantic.min.css";
 import React from "react";
-import { Container, Header, Grid, Divider } from "semantic-ui-react";
+import { Header, Grid, Divider, Image, Segment } from "semantic-ui-react";
 import "../styles/Home.css";
 
 function Home() {
   return (
-    <Grid columns={3} centered className="grid">
+    <Grid stackable columns={3} className="grid">
       <Grid.Row className="row1">
-        <Grid.Column conatiner>
-          <Divider horizontal className="slogan">
-            Become the rest without lying like the rest
-          </Divider>
+        <Grid.Column textAlign="centered" className="column1">
+          <div className="slogan">
+            <Divider horizontal>
+              Become the best without lying like the rest
+            </Divider>
+          </div>
         </Grid.Column>
       </Grid.Row>
-      <Grid.Row className="row2">
-        <Grid.Column>
-          <Container text>
-            <Header class="h1" className="title" size="large">
-              Apto
-            </Header>
-          </Container>
-        </Grid.Column>
-        <Grid.Column />
-        <Grid.Column />
+      <Grid.Row textAlign="centered" className="row2">
+        <Image
+          className="vitaminsHand"
+          src={require("../images/vitaminsHand.jpg")}
+        ></Image>
       </Grid.Row>
-      <Grid.Row className="row3">
-        <Grid.Column width={9} className="box1">
-          <Container text>
+      <Grid.Row className="row3" textAlign="centered">
+        <h3 className="apto">Apto</h3>
+      </Grid.Row>
+      <Grid.Row container stretched className="row4">
+        <Grid.Column width={3}>
+          <Image
+            className="vitaminsFalling"
+            src={require("../images/vitaminsFalling.jpg")}
+          ></Image>
+        </Grid.Column>
+        <Grid.Column width={9}>
+          <Segment basic>
             <div>
-              <Header as="h3">Whats our goal?</Header>
+              <Header as="h3" className="title">
+                Whats our goal?{" "}
+              </Header>
               <Divider section className="divide" />
               <p>
                 Apto is designed to help everyone learn more information on what
@@ -41,40 +49,35 @@ function Home() {
                 performance or health.
               </p>
             </div>
-          </Container>
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row className="row4">
-        <Grid.Column width={9} className="box2">
-          <Container text>
+          </Segment>
+          <Segment basic>
+            <Divider hidden />
+          </Segment>
+          <Segment basic>
             <div>
-              <Header as="h3">What can we do currently?</Header>
+              <Header as="h3" className="title">
+                What can we do currently?
+              </Header>
               <Divider section className="divide" />
-              <Container as="h3" textAlign="left">
-                Current Features
-              </Container>
-              <Container as="h3" textAlign="left">
-                Future Features
-              </Container>
-              <p>
-                - Reliable information on all supplements that we can get our
-                hands on<br></br>- View supplements that affect your goal!
-                <br></br>- Market information will appear showing products
-                currently being sold on the market that are recommended for
-                specific supplements or goals<br></br>- User profiles<br></br>-
-                Wishlists can be created to store supplements or goals that
-                users want to go back and see
-                <br></br>- User reviews to store opinions on supplements taken
-                <br></br>- In-depth predictive data analysis for continous use
-                of a supplement<br></br>- Features more focused towards gym
-                media for sharing and messaging other users<br></br>- Creaing a
-                custom blend of supplements to see overall effect or if it is on
-                the market already
-                <br></br>- Validating other companies' products
-                <br></br>
-              </p>
+              <Header as="h3">Current Features</Header>
+              <Header as="h3">Future Features</Header>- Reliable information on
+              all supplements that we can get our hands on
+              <br></br>- View supplements that affect your goal!
+              <br></br>- Market information will appear showing products
+              currently being sold on the market that are recommended for
+              specific supplements or goals<br></br>- User profiles
+              <br></br>- Wishlists can be created to store supplements or goals
+              that users want to go back and see
+              <br></br>- User reviews to store opinions on supplements taken
+              <br></br>- In-depth predictive data analysis for continous use of
+              a supplement<br></br>- Features more focused towards gym media for
+              sharing and messaging other users<br></br>- Creaing a custom blend
+              of supplements to see overall effect or if it is on the market
+              already
+              <br></br>- Validating other companies' products
+              <br></br>
             </div>
-          </Container>
+          </Segment>
         </Grid.Column>
       </Grid.Row>
     </Grid>
