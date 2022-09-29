9f0b472a24b550d08db5a3ac20143dcc468aacb9
Tyler Felgenhauer <tfelgs77@gmail.com>
2022-09-29 16:25:34 -0500

    Finished supplement index functions, started JSON filling

--- reactCode/src/pages/OurMission.js
+++ reactCode/src/pages/OurMission.js
@@ -2,10 +2,10 @@ import "semantic-ui-css/semantic.min.css";
 import React from "react";
 import "../images/MeetTeam.png";
 import { Container, Header, Image } from "semantic-ui-react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const hStyle = { color: 'red' };
-const element = <h1 style={ hStyle }>Developer</h1>
+const hStyle = { color: "red" };
+const element = <h1 style={hStyle}>Developer</h1>;
 function OurMission() {
   return (
     <Container text>
@@ -13,40 +13,45 @@ function OurMission() {
         Our Mission
       </Header>
       <div>
-          <p>
-            Our mission with Apto is to design an application to help everyone learn more information on what
-            supplements to take for a variety of performance or health related
-            goals. Many businesses are not explaining the basic effect or goal
-            of these supplements, but rather sayinig it will just make you look
-            like whatever influencer is marketing the product. Apto will allow
-            users to become more aware of what they are taking and the overall
-            effect it has on their actual performance or health.
-          </p>
-          <Header className = 'title' class ='h1' size = 'large'>
-            Meet The Team!
-          </Header>
-          <Header className = 'title' class = 'h3'>
-            Tyler Felgenhauer - <span className='green'> Founder</span> and <span className = 'red'> Developer</span>
-          </Header>
-          <p>
-            Tyler is a college student going to Iowa State University. He has a hobby of working out at the gym.
-            And he noticed that when trying to find the perfect supplement for his workout he noticed 
-            how much of a struggle that really is. So he took the matters into his own hands and started to create
-            an application that you can use to find the perfect supplement for you.
-          </p>
-          <Header className = 'title' class = 'h3'>
-            Dylan Felgenhauer - <span className = 'red'> Developer</span>
-          </Header>
-          <p>
-            Dylan is the twin brother of Tyler Felgenhauer and a college student that goes to Illinois 
-            Institute of Technology. He is currently studying Computer Science and is a bartender.
-          </p>
-          <Image
-                  as={Link}
-                  src={require("../images/MeetTeam.png")}
-                  to="/Home"
-          ></Image>
-        </div>
+        <p>
+          Our mission with Apto is to design an application to help everyone
+          learn more information on what supplements to take for a variety of
+          performance or health related goals. Many businesses are not
+          explaining the basic effect or goal of these supplements, but rather
+          sayinig it will just make you look like whatever influencer is
+          marketing the product. Apto will allow users to become more aware of
+          what they are taking and the overall effect it has on their actual
+          performance or health.
+        </p>
+        <Header className="title" class="h1" size="large">
+          Meet The Team!
+        </Header>
+        <Header className="title" class="h3">
+          Tyler Felgenhauer - <span className="green"> Founder</span> and{" "}
+          <span className="red"> Developer</span>
+        </Header>
+        <p>
+          Tyler is a college student going to Iowa State University. He has a
+          hobby of working out at the gym. And he noticed that when trying to
+          find the perfect supplement for his workout he noticed how much of a
+          struggle that really is. So he took the matters into his own hands and
+          started to create an application that you can use to find the perfect
+          supplement for you.
+        </p>
+        <Header className="title" class="h3">
+          Dylan Felgenhauer - <span className="red"> Developer</span>
+        </Header>
+        <p>
+          Dylan is the twin brother of Tyler Felgenhauer and a college student
+          that goes to Illinois Institute of Technology. He is currently
+          studying Computer Science and is a bartender.
+        </p>
+        <Image
+          as={Link}
+          src={require("../images/MeetTeam.png")}
+          to="/Home"
+        ></Image>
+      </div>
     </Container>
   );
 }
--- reactCode/src/pages/SupplementIndex.js
+++ reactCode/src/pages/SupplementIndex.js
@@ -1,92 +1,95 @@
 import _ from "lodash";
 import supplements from "../JSON/supplements.json";
 import React, { Component } from "react";
-import { Search, Grid, List, Segment, Header } from "semantic-ui-react";
+import { Search, Grid, List, Segment } from "semantic-ui-react";
 
-const initialState = {
-  isLoading: false,
-  results: [],
-  value: "",
-  result: [],
+let initialState = {
+	isLoading: false,
+	results: [],
+	value: "",
+	result: [],
 };
 
 export default class SearchExampleStandard extends Component {
-  state = initialState;
+	state = initialState;
 
-  handleResultSelect = (e, { result }) => {
-    this.setState({ value: result.title, result: result });
-  };
+	handleResultSelect = (e, { result }) => {
+		this.setState({ value: JSON.stringify(result.title), result: result });
+	};
 
-  handleSearchChange = (e, { value }) => {
-    this.setState({ isLoading: true, value });
+	ListSelect = (el, result) => {
+		this.setState({ result: el });
+		console.log(result);
+	};
 
-    setTimeout(() => {
-      if (this.state.value.length < 1) return this.setState(initialState);
+	handleSearchChange = (e, { value }) => {
+		this.setState({ isLoading: true, value });
 
-      const re = new RegExp(_.escapeRegExp(this.state.value), "i");
-      const isMatch = (result) => re.test(result.title);
+		setTimeout(() => {
+			if (this.state.value.length < 1) return this.setState(initialState);
 
-      this.setState({
-        isLoading: false,
-        results: _.filter(supplements, isMatch),
-      });
-    }, 300);
-  };
+			let re = new RegExp(_.escapeRegExp(this.state.value), "i");
+			let isMatch = (result) => re.test(result.title);
 
-  render() {
-    const { isLoading, value, results, result } = this.state;
+			this.setState({
+				isLoading: false,
+				results: _.filter(supplements, isMatch),
+			});
+		}, 300);
+	};
 
-    const name = JSON.stringify(result.title);
-    const group = JSON.stringify(result.group);
-    const description = JSON.stringify(result.description);
+	render() {
+		let { isLoading, value, results, result } = this.state;
 
-    return (
-      <Grid centered columns={2}>
-        <Grid.Row>
-          <Grid.Column width={3}>
-            <Search
-              placeholder="Search for a supplement..."
-              fluid
-              loading={isLoading}
-              onResultSelect={this.handleResultSelect}
-              onSearchChange={_.debounce(this.handleSearchChange, 500, {
-                leading: true,
-              })}
-              results={results}
-              value={value}
-            />
-          </Grid.Column>
-          <Grid.Column />
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column width={3}>
-            <Segment>
-              <List link>
-                {supplements.map((el) => {
-                  return (
-                    <List.Item
-                      as="a"
-                      onClick={this.handleResultSelect}
-                      loading={isLoading}
-                      results={results}
-                      value={value}
-                    >
-                      <List.Content>{el.title}</List.Content>
-                    </List.Item>
-                  );
-                })}
-              </List>
-            </Segment>
-          </Grid.Column>
-          <Grid.Column width={10}>
-            <Segment>
-              <pre>{name}</pre>
-              <pre>{group}</pre>
-              <pre>{description}</pre>
-            </Segment>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    );
-  }
+		return (
+			<Grid centered columns={2}>
+				<Grid.Row>
+					<Grid.Column width={3}>
+						<Search
+							placeholder="Search for a supplement..."
+							fluid
+							loading={isLoading}
+							onResultSelect={this.handleResultSelect}
+							onSearchChange={_.debounce(this.handleSearchChange, 500, {
+								leading: true,
+							})}
+							results={results}
+							value={value}
+						/>
+					</Grid.Column>
+					<Grid.Column />
+				</Grid.Row>
+				<Grid.Row>
+					<Grid.Column width={3}>
+						<Segment>
+							<List link>
+								{supplements.map((el) => {
+									return (
+										<List.Item
+											as="a"
+											onClick={(e) => {
+												this.ListSelect(el);
+											}}
+											results={results}
+											value={value}
+										>
+											<List.Content>{el.title}</List.Content>
+										</List.Item>
+									);
+								})}
+							</List>
+						</Segment>
+					</Grid.Column>
+					<Grid.Column width={10}>
+						<Segment basic>
+							<h3 className="title">{result.title}</h3>
+						</Segment>
+						<Segment basic>
+							<p>{result.description}</p>
+						</Segment>
+					</Grid.Column>
+				</Grid.Row>
+			</Grid>
+		);
+	}
 }
